19ebe86c75f9c16bad76cf2f71c6f2de1afbb684
Sivanathan V <104622092+Sivav15@users.noreply.github.com>
2022-11-25 15:12:39 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { switchToUser } = require("../controllers/admin/user/switchToUser");
 const { user } = require("../controllers/admin/user/users");
-// const { forgotPassword } = require("../controllers/auth/ForgotPassword");
+const { forgotPassword } = require("../controllers/auth/ForgotPassword");
 const { login } = require("../controllers/auth/Login");
 const { passwordResetPage } = require("../controllers/auth/password-reset-page");
 const  { register } = require("../controllers/auth/register");
@@ -15,7 +15,7 @@ const router = express.Router();
 // Authentication Route 
 router.post("/register",register);
 router.post("/login",login);
-// router.post("/forgot-password",forgotPassword);
+router.post("/forgot-password",forgotPassword);
 router.post("/password-reset-page",authentication,passwordResetPage);
 
 
